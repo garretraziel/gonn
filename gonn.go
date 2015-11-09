6727aa09ec0f474813ab03f7c3a542b5fb23d137
Jan Sedlák <jsedlak@redhat.com>
2015-11-09 17:29:18 +0100

    user labels as float64 slice

--- gonn.go
+++ gonn.go
@@ -12,7 +12,7 @@ func main() {
     labelPathPtr := flag.String("labels", "dataset/train-labels.idx1-ubyte", "path to MNIST labels file")
     imagesPathPtr := flag.String("images", "dataset/train-images.idx3-ubyte", "path to MNIST images file")
     flag.Parse()
-    labelsLoaded, err := mnistloader.ReadLabels(*labelPathPtr)
+    labels, err := mnistloader.ReadLabels(*labelPathPtr)
     if err != nil {
         panic(err)
     }
@@ -21,22 +21,16 @@ func main() {
         panic(err)
     }
 
-    labels, err := matrices.InitMatrixWithValues(1, len(labelsLoaded), labelsLoaded)
-    if err != nil {
-        panic(err)
-    }
+    inputLength := 0
     images := make([]matrices.Matrix, len(imagesLoaded))
     for i, val := range imagesLoaded {
         images[i], err = matrices.InitMatrixWithValues(1, len(val), val)
+        inputLength = len(val)
         if err != nil {
             panic(err)
         }
     }
-    _ = labels
 
-    network := nn.InitNN([]int{4, 2, 3})
-    fmt.Println(network)
-    input, _ := matrices.InitMatrixWithValues(1, 4, []float64{1, 1, 1, 1})
-    output := network.FeedForward(input)
-    fmt.Println(output)
+    network := nn.InitNN([]int{inputLength, 30, 10})
+    fmt.Println(network.Evaluate(images, labels))
 }
